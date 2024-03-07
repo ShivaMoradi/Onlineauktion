417890e1838d446e3379ec2c2d8eb47dfe3a11c8
ShivaMoradi <shiva.moradi1@gmail.com>
2024-03-07 13:01:04 +0100

    Added function to show alert meesage for successful or failed ragistration

--- src/components/NewUser.jsx
+++ src/components/NewUser.jsx
@@ -1,23 +1,31 @@
 
-function NewUser ( event ) {
+async function NewUser ( event ) {
  
     event.preventDefault()
     const data = new FormData( event.target )
-      const info = Object.fromEntries( data )
-      fetch( "/api/users", {
-        method: "POST", // or 'PUT'
+  const info = Object.fromEntries( data )
+  try {
+    const response = await fetch( "/api/users", {
+      method: "POST",
       headers: {
         "Content-Type": "application/json",
-    },
+      },
       body: JSON.stringify( info ),
 
-  } );
+    } )
+    if (response.ok) {
+      alert("Successfully registerd!")
+    } else {
+      alert("Failed! Please try again.")
+    }
+    
       
-
-   
+  } catch ( error ) {
+    console.error( "Error during registration:", error )
+    alert( "An error occurred. Please try again later." )
+  }
   
-      
-}
+  }  
 
 /*const Success = () => {
   alert("sucess")
