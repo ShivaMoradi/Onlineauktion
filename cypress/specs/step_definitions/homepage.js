59fdf94b166dbe75525e1d3d04ddd6863595c82b
gentlecub <118164637+gentlecub@users.noreply.github.com.>
2024-05-16 01:05:25 +0200

    cypress testing

--- cypress/specs/step_definitions/homepage.js
+++ cypress/specs/step_definitions/homepage.js
@@ -4,10 +4,6 @@ Given("I am on the {string} page", (a) => {
   cy.visit("/");
 });
 
-When("the page loads", () => {
-  cy.wait("http://localhost:5173").its("response.statusCode").should("eq", 200);
-});
-
 Then("I should then see the price of the car.", () => {
   cy.getDataTest("cotainercars").its("length").should("be.gt", 0);
 });
